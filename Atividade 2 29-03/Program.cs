5bdb4fc26c18719e29cdf3f93918c7d6119594a4
SMKGui <125401287+SMKGui@users.noreply.github.com>
2023-03-29 17:20:51 -0300

    Add files via upload

---
+++ Atividade 2 29-03/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+Console.WriteLine($"Em que ano nasceu?");
+int ano = int.Parse(Console.ReadLine());
+
+int idade = 2023 - ano;
+Console.WriteLine($"Sua idade: {idade}");
---
+++ Atividade 29-03/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+// Programa para calcular idade
+
+Console.WriteLine($"Digite sua idade");
+
+int idade = int.Parse(Console.ReadLine());
+
+int idadeEmMeses = idade * 12;
+Console.WriteLine($"Sua idade em meses: {idadeEmMeses} ");
+
+int IdadeEmSemanas = idade * 52;
+Console.WriteLine($"Sua idade em semanas: {IdadeEmSemanas}");
+
+int IdadeEmDias = idade * 365;
+Console.WriteLine($"Sua idade em dias: {IdadeEmDias}");
+
+int IdadeEmHoras = idade * 8760;
+Console.WriteLine($"Sua idade em horas: {IdadeEmHoras}");
+
+int IdadeEmMinutos = idade * 525600;
+Console.WriteLine($"Sua idade em minutos: {IdadeEmMinutos}");
+
